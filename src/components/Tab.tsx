269f7b7c9878ac82d93fc7224a00c10bb2e80e80
chichi <seonrye.yun@gmail.com>
2023-08-13 21:52:33 +0900

    fix: li 중첩 콘솔 로그 오류 수정(Warning: validateDOMNesting(...): <li> cannot appear as a descendant of <li>)

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const HeaderStyle = styled.div`
@@ -12,43 +12,45 @@ const TitleBox = styled.h1`
   font-size: 30px;
   margin-bottom: 20px;
   font-family: "Walter Turncoat", cursive;
-  font-size: 35px;
+  font-size: 22px;
 `;
 
 const MenuBox = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(3, 1fr);
   gap: 10px;
 `;
 
 const MenuIconStyle = styled.li`
   width: 60px;
   height: 60px;
-  border: 2px solid #f0f0f0;
-  border-radius: 30%;
+  border: 1.3px solid #f0f0f0;
+  border-radius: 35%;
   display: grid;
   justify-content: center;
   align-content: center;
   background-color: #fdfdfd;
   color: #727272;
   font-family: "Walter Turncoat", cursive;
+  font-size: 0.8rem;
 `;
 
 const Tab = () => {
   return (
     <HeaderStyle>
-      <TitleBox>Practice for Recoil</TitleBox>
+      <TitleBox>Practice for Recoil & React Query</TitleBox>
       <MenuBox>
-        <li>
-          <MenuIconStyle>
-            <Link to="/input">Input</Link>
-          </MenuIconStyle>
-        </li>
-        <li>
-          <MenuIconStyle>
-            <Link to="/list">Todo</Link>
-          </MenuIconStyle>
-        </li>
+        <MenuIconStyle>
+          <Link to="/input">Input</Link>
+        </MenuIconStyle>
+
+        <MenuIconStyle>
+          <Link to="/list">Todo</Link>
+        </MenuIconStyle>
+
+        <MenuIconStyle>
+          <Link to="/devtools">DevTools</Link>
+        </MenuIconStyle>
       </MenuBox>
     </HeaderStyle>
   );
