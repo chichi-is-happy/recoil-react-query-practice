1d59f6916ad12b78b88a239c2af7bfaaec9d9c09
chichi <seonrye.yun@gmail.com>
2023-08-13 21:48:49 +0900

    fix: styled component 콘솔 로그 에러 수정(You may see this warning because you've called styled inside another component)

--- src/App.tsx
+++ src/App.tsx
@@ -3,47 +3,48 @@ import { RecoilRoot } from "recoil";
 import TextInput from "./components/InputTest/TextInput";
 import CharacterCounter from "./components/InputTest/CharacterCounter";
 import "./App.css";
-import TodoList from "../src/components/Todo/TodoList";
+import TodoList from "./components/RecoilTodo/TodoList";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Tab from "./components/Tab";
 import styled from "styled-components";
+import DevTools from "./components/ReactQueryTodo/DevTools";
+// import { QueryClient, QueryClientProvider } from "react-query";
+// import { ReactQueryDevtools } from "react-query/devtools";
+const ContainerStyle = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #fdfdfd;
+  display: grid;
+  justify-content: center;
+  /* grid-template-rows: auto 1fr auto; */
+`;
 
+const MobileStyle = styled.div`
+  width: 480px;
+  border: 10px solid #fff3f7;
+  border-radius: 10%;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+`;
+
+const HeaderStyle = styled.header`
+  padding: 1rem;
+  background-color: #fae2ea;
+  color: white;
+  display: grid;
+  align-items: center;
+`;
+
+const ContentStyle = styled.div`
+  padding: 1rem;
+  display: grid;
+  justify-content: center;
+  justify-items: center;
+  align-items: center;
+  font-family: "Walter Turncoat", cursive;
+`;
 const App = () => {
-  const ContainerStyle = styled.div`
-    width: 100%;
-    min-height: 100vh;
-    background-color: #fdfdfd;
-    display: grid;
-    justify-content: center;
-    /* grid-template-rows: auto 1fr auto; */
-  `;
-
-  const MobileStyle = styled.div`
-    width: 480px;
-    border: 10px solid #fff3f7;
-    border-radius: 10%;
-    align-items: center;
-    justify-content: center;
-    overflow: hidden;
-  `;
-
-  const HeaderStyle = styled.header`
-    padding: 1rem;
-    background-color: #fae2ea;
-    color: white;
-    display: grid;
-    align-items: center;
-  `;
-
-  const ContentStyle = styled.div`
-    padding: 1rem;
-    display: grid;
-    justify-content: center;
-    justify-items: center;
-    align-items: center;
-    font-family: "Walter Turncoat", cursive;
-  `;
-
   return (
     <Router>
       <ContainerStyle>
@@ -53,12 +54,11 @@ const App = () => {
           </HeaderStyle>
           <ContentStyle>
             <RecoilRoot>
-              <div>
-                <Routes>
-                  <Route path="/input" element={<TextInput />} />
-                  <Route path="/list" element={<TodoList />} />
-                </Routes>
-              </div>
+              <Routes>
+                <Route path="/input" element={<TextInput />} />
+                <Route path="/list" element={<TodoList />} />
+                <Route path="/devtools" element={<DevTools />} />
+              </Routes>
             </RecoilRoot>
           </ContentStyle>
         </MobileStyle>
