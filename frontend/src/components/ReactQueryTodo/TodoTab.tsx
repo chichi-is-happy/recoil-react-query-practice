2170c226cada6e6a2348397cc1007fb52127694a
chichi <seonrye.yun@gmail.com>
2023-08-20 20:17:33 +0900

    feat: All Open Closed 탭을 select에서 가로형(div)으로 볼 수 있도록 변경, CSS 수정

--- frontend/src/components/ReactQueryTodo/TodoTab.tsx
+++ frontend/src/components/ReactQueryTodo/TodoTab.tsx
@@ -1,22 +1,43 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useRecoilState } from "recoil";
 import { todoFilterState } from "../../../src/recoilState/todoState";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-items: center;
+  font-size: 1.2rem;
+  border-bottom: 1px solid #dee2e6;
+  margin-bottom: 1rem;
+  padding-bottom: 1rem;
+  color: #727272;
+  cursor: default;
+`;
 
 const TodoTab = () => {
   const [todoList, setTodoList] = useRecoilState(todoFilterState);
 
-  const updateFilter = ({ target: { value } }: any) => {
-    setTodoList(value);
+  const updateFilter = (event: React.MouseEvent<HTMLDivElement>) => {
+    const target = event.currentTarget;
+    const value = target.getAttribute("data-value");
+    if (value) {
+      setTodoList(value);
+    }
   };
 
   return (
-    <>
-      <select value={todoList} onChange={updateFilter}>
-        <option value="All">All</option>
-        <option value="Open">Open</option>
-        <option value="Closed">Closed</option>
-      </select>
-    </>
+    <Container>
+      <div data-value="All" onClick={updateFilter}>
+        All
+      </div>
+      <div data-value="Open" onClick={updateFilter}>
+        Open
+      </div>
+      <div data-value="Closed" onClick={updateFilter}>
+        Closed
+      </div>
+    </Container>
   );
 };
 
